306819e10b6ab343e1bb45e90071305a65fb5eb2
th0t <julien.vanderstock@protonmail.ch>
2020-06-22 18:33:57 +0200

    Refactored spec + added comments about what I did (in 30 mins) - totaling 2 hours of effective work

--- rect.rb
+++ rect.rb
@@ -1,21 +1,29 @@
 class Rectangle
   def initialize(x, y, la, ra, bc, mc, reverse_last_angles)
     @x = x; @y = y
-    raise 'Exepecting dimensions as Integer' unless @x.is_a?(Integer) || @y.is_a?(Integer)
+    raise 'Exepecting dimensions as Integer' unless @x.is_a?(Integer) && @y.is_a?(Integer)
 
     @la = la; @ra = ra; @bc = bc; @mc = mc
-    raise 'Exepecting single caracters' unless @la.is_a?(String) || @ra.is_a?(String) || @bc.is_a?(String) || @mc.is_a?(String)
-    raise 'Exepecting single caracters' unless @la.size == 1 || @ra.size == 1 || @bc.size == 1 || @mc.size == 1
+    raise 'Exepecting single caracters' unless @la.is_a?(String) && @ra.is_a?(String) && @bc.is_a?(String) && @mc.is_a?(String)
+    raise 'Exepecting single caracters' unless @la.size == 1 && @ra.size == 1 && @bc.size == 1 && @mc.size == 1
 
     @reverse_last_angles = reverse_last_angles
     raise 'Expecting reverse_last_angles to be a boolean' unless !!@reverse_last_angles == @reverse_last_angles
 
     @output = []
+
+    # Here I could refacto further more initializer, for exemples passing an array for dimensions and caracters
+    # It would simplify guards conditions, but I don't think it worth my time
+    # I should rename my parameters too, (yeah I'm a lazy person)
   end
 
   def make_payload
+    @output = [] # Just there for several call before display
+    # Managing zero case here as it does make sense to me (zero is void, not to be treated as an error)
     return if @x.zero? || @y.zero?
 
+    # Here a single line output it dealt by design, not explicit
+    # I could start the index at 1 but I prefer machine based view
     @y.times do |index|
       if first_line?(index)
         @output << make_first_line
@@ -28,15 +36,29 @@ class Rectangle
     @output
   end
 
+  def draw_payload
+    # Can't be broken I think, juste making payload if not already created
+    make_payload if @output.empty?
+
+    @output.each do |line|
+      puts line
+    end
+  end
+
+  private
+
   def first_line?(index)
     index == 0 ? true : false
   end
 
   def last_line?(index)
+    # First line can also be last line when single row, not a problem in my case
     index == @y-1 ? true : false
   end
 
   def make_first_line
+    # Single colomns management dealt here
+    # (May be refactored)
     line = @bc * @x
     line[0] = @la
     line[-1] = @ra unless @x == 1
@@ -44,6 +66,7 @@ class Rectangle
   end
 
   def make_middle_line
+    # Works great on single colomn too
     line = @mc * @x
     line[0] = @bc
     line[-1] = @bc
@@ -51,6 +74,7 @@ class Rectangle
   end
 
   def make_last_line
+    # Single colomns management dealt here + reverse last line
     line = @bc * @x
     if @reverse_last_angles
       line[0] = @ra
@@ -62,15 +86,8 @@ class Rectangle
     line
   end
 
-  def draw_payload
-    make_payload if @output.empty?
-
-    @output.each do |line|
-      puts line
-    end
-  end
-
   def self.demo_parcours
+    # Reproducing exemples
     puts 'XXXXXXXXXXXX'
     puts '------------'
     Rectangle.new(5, 4, 'o', 'o', '-', ' ', false).draw_payload
--- rect_spec.rb
+++ rect_spec.rb
@@ -4,7 +4,48 @@ RSpec.configure do |config|
   config.formatter = :documentation
 end
 
+shared_examples "a rectangle" do
+  context 'single array' do
+    let(:x) { 1 }
+    let(:y) { 1 }
+    it 'returns the left border only' do
+      expect(@subject.make_payload).to eq([left_angle_caracter])
+    end
+  end
+  context 'single line' do
+    let(:x) { 3 }
+    let(:y) { 1 }
+    it 'returns a single line' do
+      expect(@subject.make_payload).to eq(["#{left_angle_caracter}#{border_caracter}#{right_angle_caracter}"])
+    end
+  end
+  context 'single column' do
+    let(:x) { 1 }
+    let(:y) { 3 }
+    it 'returns 3 lines' do
+      if reverse_last_angles
+        expect(@subject.make_payload).to eq([left_angle_caracter, border_caracter, right_angle_caracter])
+      else
+        expect(@subject.make_payload).to eq([left_angle_caracter, border_caracter, left_angle_caracter])
+      end
+    end
+  end
+  context 'square matrix' do
+    let(:x) { 3 }
+    let(:y) { 3 }
+    it 'returns consistant midle line' do
+      expect(@subject.make_payload[1]).to eq("#{border_caracter}#{middle_caracter}#{border_caracter}")
+    end
+  end
+end
+
+# I will not test error cases or private methods (I test only the valuable parts, error mgmt was not requested, nor edge cases)
+
 describe Rectangle do
+  before(:each) do
+    @subject = Rectangle.new(x, y, left_angle_caracter, right_angle_caracter, border_caracter, middle_caracter, reverse_last_angles)
+  end
+
   context 'General testing' do
     let(:border_caracter) { '-' }
     let(:middle_caracter) { ' ' }
@@ -14,92 +55,14 @@ describe Rectangle do
       let(:left_angle_caracter) { 'o' }
       let(:right_angle_caracter) { 'o' }
 
-      context 'single array' do
-        let(:x) { 1 }
-        let(:y) { 1 }
-        before do
-          @subject = Rectangle.new(x, y, left_angle_caracter, right_angle_caracter, border_caracter, middle_caracter, reverse_last_angles)
-        end
-        it 'returns the left border only' do
-          expect(@subject.make_payload).to eq([left_angle_caracter])
-        end
-      end
-      context 'single line' do
-        let(:x) { 3 }
-        let(:y) { 1 }
-        before do
-          @subject = Rectangle.new(x, y, left_angle_caracter, right_angle_caracter, border_caracter, middle_caracter, reverse_last_angles)
-        end
-        it 'returns a single line' do
-          expect(@subject.make_payload).to eq(["#{left_angle_caracter}#{border_caracter}#{right_angle_caracter}"])
-        end
-      end
-      context 'single column' do
-        let(:x) { 1 }
-        let(:y) { 3 }
-        before do
-          @subject = Rectangle.new(x, y, left_angle_caracter, right_angle_caracter, border_caracter, middle_caracter, reverse_last_angles)
-        end
-        it 'returns 3 lines' do
-          expect(@subject.make_payload).to eq([left_angle_caracter, border_caracter, left_angle_caracter])
-        end
-      end
-      context 'square matrix' do
-        let(:x) { 3 }
-        let(:y) { 3 }
-        before do
-          @subject = Rectangle.new(x, y, left_angle_caracter, right_angle_caracter, border_caracter, middle_caracter, reverse_last_angles)
-        end
-        it 'returns consistant midle line' do
-          expect(@subject.make_payload[1]).to eq("#{border_caracter}#{middle_caracter}#{border_caracter}")
-        end
-      end
+      it_behaves_like "a rectangle"
     end
 
     context 'when angle caracters are not the same' do
       let(:left_angle_caracter) { 'A' }
       let(:right_angle_caracter) { 'B' }
 
-      context 'single array' do
-        let(:x) { 1 }
-        let(:y) { 1 }
-        before do
-          @subject = Rectangle.new(x, y, left_angle_caracter, right_angle_caracter, border_caracter, middle_caracter, reverse_last_angles)
-        end
-        it 'returns the left border only' do
-          expect(@subject.make_payload).to eq([left_angle_caracter])
-        end
-      end
-      context 'single line' do
-        let(:x) { 3 }
-        let(:y) { 1 }
-        before do
-          @subject = Rectangle.new(x, y, left_angle_caracter, right_angle_caracter, border_caracter, middle_caracter, reverse_last_angles)
-        end
-        it 'returns a single line' do
-          expect(@subject.make_payload).to eq(["#{left_angle_caracter}#{border_caracter}#{right_angle_caracter}"])
-        end
-      end
-      context 'single column' do
-        let(:x) { 1 }
-        let(:y) { 3 }
-        before do
-          @subject = Rectangle.new(x, y, left_angle_caracter, right_angle_caracter, border_caracter, middle_caracter, reverse_last_angles)
-        end
-        it 'returns 3 lines' do
-          expect(@subject.make_payload).to eq([left_angle_caracter, border_caracter, left_angle_caracter])
-        end
-      end
-      context 'square matrix' do
-        let(:x) { 3 }
-        let(:y) { 3 }
-        before do
-          @subject = Rectangle.new(x, y, left_angle_caracter, right_angle_caracter, border_caracter, middle_caracter, reverse_last_angles)
-        end
-        it 'returns consistant midle line' do
-          expect(@subject.make_payload[1]).to eq("#{border_caracter}#{middle_caracter}#{border_caracter}")
-        end
-      end
+      it_behaves_like "a rectangle"
     end
 
     context 'when last line must be reversed' do
@@ -107,46 +70,7 @@ describe Rectangle do
       let(:right_angle_caracter) { 'B' }
       let(:reverse_last_angles) { true }
 
-      context 'single array' do
-        let(:x) { 1 }
-        let(:y) { 1 }
-        before do
-          @subject = Rectangle.new(x, y, left_angle_caracter, right_angle_caracter, border_caracter, middle_caracter, reverse_last_angles)
-        end
-        it 'returns the left border only' do
-          expect(@subject.make_payload).to eq([left_angle_caracter])
-        end
-      end
-      context 'single line' do
-        let(:x) { 3 }
-        let(:y) { 1 }
-        before do
-          @subject = Rectangle.new(x, y, left_angle_caracter, right_angle_caracter, border_caracter, middle_caracter, reverse_last_angles)
-        end
-        it 'returns a single line' do
-          expect(@subject.make_payload).to eq(["#{left_angle_caracter}#{border_caracter}#{right_angle_caracter}"])
-        end
-      end
-      context 'single column' do
-        let(:x) { 1 }
-        let(:y) { 3 }
-        before do
-          @subject = Rectangle.new(x, y, left_angle_caracter, right_angle_caracter, border_caracter, middle_caracter, reverse_last_angles)
-        end
-        it 'returns 3 lines' do
-          expect(@subject.make_payload).to eq([left_angle_caracter, border_caracter, right_angle_caracter])
-        end
-      end
-      context 'square matrix' do
-        let(:x) { 3 }
-        let(:y) { 3 }
-        before do
-          @subject = Rectangle.new(x, y, left_angle_caracter, right_angle_caracter, border_caracter, middle_caracter, reverse_last_angles)
-        end
-        it 'returns consistant midle line' do
-          expect(@subject.make_payload[1]).to eq("#{border_caracter}#{middle_caracter}#{border_caracter}")
-        end
-      end
+      it_behaves_like "a rectangle"
     end
   end
 end
